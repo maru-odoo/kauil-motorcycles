4bacd2ca0f80067a7586d4576f6b6966e549cec8
Matthew Rubino <maru@odoo.com>
2023-06-21 14:05:09 -0400

    Add default values

--- ge1/model/stock_lot.py
+++ ge1/model/stock_lot.py
@@ -1,4 +1,4 @@
-from odoo import models, api, fields
+from odoo import models
 
 
 class StockLot(models.Model):
@@ -6,15 +6,14 @@ class StockLot(models.Model):
 
     def _get_next_serial(self, company, product):
         tmpl = product.product_tmpl_id
-        print(tmpl)
-        if tmpl.detailed_type == 'motorcycle':
-            make = tmpl.make[:2].upper()
-            model = tmpl.model[:2].upper()
-            year = tmpl.year%100
-            battery_capacity = tmpl.battery_capacity[:2].upper()
+
+        if tmpl.detailed_type == 'motorcycle' and product.tracking != "none":
+            make = tmpl.make[:2].upper() if tmpl.make else 'XX'
+            model = tmpl.model[:2].upper() if tmpl.model else 'XX'
+            year = tmpl.year%100 if tmpl.year else 'XX'
+            battery_capacity = tmpl.battery_capacity[:2].upper() if tmpl.battery_capacity else 'XX'
             serial_number = self.env["ir.sequence"].next_by_code("stock.lot.serial")
 
-            print(make,model, year, battery_capacity)
             return f"{make}{model}{year}{battery_capacity}{serial_number}"
         else:
             return super(StockLot, self)._get_next_serial(company, product)

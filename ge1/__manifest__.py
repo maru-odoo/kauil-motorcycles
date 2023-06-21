fa9fb5f5c8c59bbfc492f21088a742db43a4cad3
jich-odoo <jich@odoo.com>
2023-06-21 13:54:53 -0400

    GE01 Completed

--- ge1/__manifest__.py
+++ ge1/__manifest__.py
@@ -4,7 +4,6 @@
     "depends": ["motorcycle_registry"],
     "category": "Kauil/Registry",
     "data": [
-        "data/motorcycle_registry_id.xml"
     ],
     "auto_install": True,
 }
--- ge1/model/stock_lot.py
+++ ge1/model/stock_lot.py
@@ -4,17 +4,17 @@ from odoo import models, api, fields
 class StockLot(models.Model):
     _inherit = "stock.lot"
 
-    name = fields.Char('Lot/Serial Number', compute='_compute_name')
+    def _get_next_serial(self, company, product):
+        tmpl = product.product_tmpl_id
+        print(tmpl)
+        if tmpl.detailed_type == 'motorcycle':
+            make = tmpl.make[:2].upper()
+            model = tmpl.model[:2].upper()
+            year = tmpl.year%100
+            battery_capacity = tmpl.battery_capacity[:2].upper()
+            serial_number = self.env["ir.sequence"].next_by_code("stock.lot.serial")
 
-    @api.depends("product_id")
-    def _compute_name(self):
-        if self.product_id.product_tmpl_id.detailed_type == 'motorcycle':
-            tmpl = self.product_id.product_tmpl_id
-            
-            # TODO: Make this readable
-            self.name = f"{tmpl.make[:2].upper()}{tmpl.model[:2].upper()}{tmpl.year%100}{tmpl.battery_capacity.upper()}{self.env['ir.sequence'].next_by_code('motorcycle_register.id')}"
-            print("Motorcycle: " + self.name)
+            print(make,model, year, battery_capacity)
+            return f"{make}{model}{year}{battery_capacity}{serial_number}"
         else:
-            # TODO: Look into repeated increment :D
-            self.name = self.env['ir.sequence'].next_by_code('stock.lot.serial')
-            print("Non-motorcycle: " + self.name)
+            return super(StockLot, self)._get_next_serial(company, product)

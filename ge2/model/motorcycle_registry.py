8860426048c4f5b850a8b4ea1e913cd6f7eadc5a
Matthew Rubino <maru@odoo.com>
2023-06-22 09:18:58 -0400

    Enforce One2One relation

--- ge2/model/motorcycle_registry.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class MotorcycleRegistry(models.Model):
-    _inherit = "motorcycle.registry"
-
-    lot_ids = fields.One2many("stock.lot", inverse_name="registry")
---
+++ ge2/models/motorcycle_registry.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class MotorcycleRegistry(models.Model):
+    _inherit = "motorcycle.registry"
+
+    lot_ids = fields.One2many("stock.lot", inverse_name="registry_id")
+    lot_id = fields.Many2one("stock.lot", compute="_compute_lot_id")
+
+    @api.constrains("lot_ids")
+    def _check_lot_ids(self):
+        lots = [x for x in self if x.lot_ids]
+        if len(lots) > 1:
+            raise ValidationError('Odoopsie! A registered motorcycle can only belong to one lot.')
+
+    def _compute_lot_id(self):
+        if len(self.lot_ids) > 0:
+            lot_id = self.lot_ids[0]
+        else:
+            lot_id = False

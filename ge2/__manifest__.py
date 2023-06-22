5b3fb7f5172531364dba1204c5e0dafb3f953dbb
elct <elct@odoo.com>
2023-06-22 10:47:31 -0400

    GE2-Lot_ID_Inherit

--- ge2/__manifest__.py
+++ ge2/__manifest__.py
@@ -1,10 +1,11 @@
 {
-    "name": "Motorcycle Registry Enhancements",
+    "name": "testMotorcycle Registry Enhancements",
     "license": "OPL-1",
     "depends": ["motorcycle_registry"],
     "category": "Kauil/Registry",
     "data": [
-        "views/stock_lot_inherit.xml"
+        "views/stock_lot_inherit.xml",
+        "views/motorcycle_registry_tree_view_inherit.xml"
     ],
     "auto_install": True,
 }

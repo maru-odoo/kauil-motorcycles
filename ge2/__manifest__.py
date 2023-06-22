1e9129b7f40814e65f9b7605434d5dec92044706
Matthew Rubino <maru@odoo.com>
2023-06-22 09:51:37 -0400

    Add registry ID to stock lot tree view

--- ge2/__manifest__.py
+++ ge2/__manifest__.py
@@ -3,5 +3,8 @@
     "license": "OPL-1",
     "depends": ["motorcycle_registry"],
     "category": "Kauil/Registry",
+    "data": [
+        "views/stock_lot_inherit.xml"
+    ],
     "auto_install": True,
 }

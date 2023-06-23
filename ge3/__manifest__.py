56e0c206c0899c2e5604c47743ea2cb082ce1b5f
Matthew Rubino <maru@odoo.com>
2023-06-23 11:52:51 -0400

    Set up module

---
+++ ge3/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Motorcycle Registry Portal",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry"],
+    "category": "Kauil/Registry",
+    "data": [],
+    "auto_install": True,
+}

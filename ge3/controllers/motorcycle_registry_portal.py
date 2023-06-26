2b763ef941a5b8d450500d5840bd7d8d24a47b1b
Matthew Rubino <maru@odoo.com>
2023-06-26 10:44:37 -0400

    Add basic portal list view

--- ge3/controllers/motorcycle_registry_portal.py
+++ ge3/controllers/motorcycle_registry_portal.py
@@ -5,13 +5,12 @@ from odoo.addons.portal.controllers import portal
 
 class MotorcycleRegistryPortal(portal.CustomerPortal):
 
-    @http.route(['/my/registrations', '/my/registrations/<string:id>'], type='http', auth='user', website=True)
+    @http.route(['/my/registrations', '/my/registrations/<int:id>'], type='http', auth='user', website=True)
     def portal_my_registrations(self, **kwargs):
         registration_id = kwargs.get("id")
         if registration_id:
-            registration = request.env['motorcycle.registry'].browse(registration_id)
-            values = {"registration": registration}
-
-            return request.render("ge3.portal_my_registration", values)
+            registration = request.env['motorcycle.registry'].browse([registration_id])
+            return request.render("ge3.portal_my_registration", {"registration": registration})
         else:
-            return request.render("ge3.portal_my_registrations")
+            registrations = request.env['motorcycle.registry'].search([])
+            return request.render("ge3.portal_my_registrations", {"registrations": registrations})
--- ge3/models/motorcycle_registry.py
+++ ge3/models/motorcycle_registry.py
@@ -1,7 +1,10 @@
 from odoo import models
 
 
-# class MotorcycleRegistry(models.Model):
-#     _inherit = ["motorcycle.registry", "portal.mixin"]
+class MotorcycleRegistry(models.Model):
+    _inherit = "motorcycle.registry"
 
-
+    def get_portal_url(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
+        self.ensure_one()
+        url = f"/my/registrations/{self.id}"
+        return url

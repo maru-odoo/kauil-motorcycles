309551b92d5274c418b2fd253b11ff73c41cfcd8
elct <elct@odoo.com>
2023-06-26 00:20:42 -0400

    Base for ge8

---
+++ ge8/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Vin For Invoice",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry"],
+    "category": "Kauil/Registry",
+    "data":["views/invoice_inherit.xml"],
+    "auto_install": True,
+}

c2665efddddd3f25e6f622eb555fb526c9c28a62
Matthew Rubino <maru@odoo.com>
2023-06-26 11:31:44 -0400

    Add list view sorting

--- ge3/controllers/motorcycle_registry_portal.py
+++ ge3/controllers/motorcycle_registry_portal.py
@@ -1,16 +1,40 @@
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 from odoo.addons.portal.controllers import portal
 
 
 class MotorcycleRegistryPortal(portal.CustomerPortal):
 
+    def _get_registry_searchbar_sortings(self):
+        return {
+            'registry_number': {'label': _('Registry #'), 'order': 'registry_number'},
+            'vin': {'label': _('VIN'), 'order': 'vin'},
+            'make': {'label': _('Make'), 'order': 'make'},
+            'model': {'model': _('Model'), 'order': 'model'},
+        }
+
     @http.route(['/my/registrations', '/my/registrations/<int:id>'], type='http', auth='user', website=True)
     def portal_my_registrations(self, **kwargs):
         registration_id = kwargs.get("id")
         if registration_id:
             registration = request.env['motorcycle.registry'].browse([registration_id])
-            return request.render("ge3.portal_my_registration", {"registration": registration})
+
+            values = {
+                "registration": registration,
+                "default_url": "/my/registrations",
+            }
+
+            return request.render("ge3.portal_my_registration", values)
         else:
             registrations = request.env['motorcycle.registry'].search([])
-            return request.render("ge3.portal_my_registrations", {"registrations": registrations})
+            searchbar_sortings = self._get_registry_searchbar_sortings()
+            sortby = kwargs.get("sortby", "registry_number")
+
+            values = {
+                "registrations": registrations,
+                "default_url": "/my/registrations",
+                "searchbar_sortings": searchbar_sortings,
+                "sortby": sortby,
+            }
+
+            return request.render("ge3.portal_my_registrations", values)
--- ge3/models/motorcycle_registry.py
+++ ge3/models/motorcycle_registry.py
@@ -1,8 +1,10 @@
-from odoo import models
+from odoo import models, fields
 
 
 class MotorcycleRegistry(models.Model):
     _inherit = "motorcycle.registry"
+
+    public = fields.Boolean(default=False)
 
     def get_portal_url(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
         self.ensure_one()

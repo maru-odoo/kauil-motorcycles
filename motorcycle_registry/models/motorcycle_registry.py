b721670629f56679aea5f41745b42c720b9d05f9
jich-odoo <jich@odoo.com>
2023-06-27 13:40:22 -0400

    GE11 Completed

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -31,6 +31,7 @@ class MotorcycleRegistry(models.Model):
     # Vehicles fields
     make = fields.Char(compute='_compute_from_vin')
     model = fields.Char(compute='_compute_from_vin')
+    brand = fields.Char(compute='_compute_from_vin')
 
     # This method works, but it's better to filter the recordset before iterating over it, like in the example below                
     # @api.constrains('license_plate')
@@ -77,6 +78,8 @@ class MotorcycleRegistry(models.Model):
         for registry in registries_with_vin:
             registry.make = registry.vin[:2]
             registry.model = registry.vin[2:4]
+            registry.brand = registry.vin[4:6]
         for registry in (self - registries_with_vin):
             registry.make = False
             registry.model = False
+            registry.brand

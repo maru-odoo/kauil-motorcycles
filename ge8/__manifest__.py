5c32571816e1b78ae67892fd838f76f42ce92fd5
elct <elct@odoo.com>
2023-06-27 13:50:22 -0400

    [ge8 progress]

--- ge8/__manifest__.py
+++ ge8/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Motorcycle Registry Invoice",
     "license": "OPL-1",
-    "depends": ["motorcycle_registry"],
+    "depends": ["motorcycle_registry","account"],
     "category": "Kauil/Registry",
     "data":["views/account_move_inherit.xml"],
     "auto_install": True,
--- ge8/models/account_move.py
+++ ge8/models/account_move.py
@@ -3,7 +3,10 @@ from odoo import models, fields
 
 class AccountMove(models.Model):
     _inherit = "account.move.line"
+    vin = fields.Char(string='VIN',compute="_compute_vin")
+    # lot_id=fields.Char(related="motorcycle_registry.lot_id")
 
-    # vin = fields.Char(comodel="motorcycle.registry",related="lot_id.name",string='VIN')
-    vin = fields.Char(string='VIN')
-    #many2one
+    def _compute_vin(self):
+        self.vin='000'
+        # for product in self:
+        #     self.vin = '000'
